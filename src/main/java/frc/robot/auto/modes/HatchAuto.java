3ddb65ea8bd5dfe19ef19337c5c64965e226d1da
FRC Team 686, Bovine Intervention <firstteam686@gmail.com>
2019-01-24 20:00:02 -0500

    made comments on "auto" tasks

--- src/main/java/frc/robot/auto/modes/HatchAuto.java
+++ src/main/java/frc/robot/auto/modes/HatchAuto.java
@@ -17,8 +17,18 @@ public class HatchAuto extends AutoModeBase {
     @Override
     protected void routine() throws AutoModeEndedException 
     {
-         runAction(new DriveStraightAction(0, 0)); 
-         runAction(new DeployHatchAction());
+        // runAction(new DriveStraightAction(0, 0)); can set drive parameters this way if driving straight 
+        //drive off platform 
+        //drive to cargoship - john
+        //turn towards cargoship - john
+        // turn on vision & line sensor to line up with reflective and white tape
+        //drive forward to close distance
+        //deploy hatch action
+        // turn over to jhon to get another hatch and drive to ship
+        //turn on vision & line sensor to line up with reflective and white tape
+        //drive forward to close distance
+        //deploy hatch action
+        runAction(new DeployHatchAction());
          runAction(new HatchResetAction());
          runAction(new PathFollowerAction(turnStationPath));    
          runAction(new PathFollowerAction(driveToHatchPath)); 
